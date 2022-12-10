9520ed76dd584ef96a17e164c173478106c7ddbe
ekaras <key.cpp@gmail.com>
2022-12-10 23:02:21 +0300

    1 coomit

---
+++ SegmentSwiftUI/SegmentSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentSwiftUIApp.swift
+//  SegmentSwiftUI
+//
+//  Created by Евгений Карась on 10.12.22.
+//
+
+import SwiftUI
+
+@main
+struct SegmentSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
