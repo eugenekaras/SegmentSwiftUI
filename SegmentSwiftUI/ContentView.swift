a145ea5809beedcc24949ea44d5867e55cc356d7
ekaras <key.cpp@gmail.com>
2022-12-11 21:55:14 +0300

    2 commit

--- SegmentSwiftUI/ContentView.swift
+++ SegmentSwiftUI/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var segmentIndex = 0
+    @State var offSetX = 0
+    
+    var company = ["Nike", "Puma", "Reebok"]
+    var sneakers = ["w1","w4","w3"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            ZStack{
+                Text("Кросовки-\(company[segmentIndex])")
+                    .font(Font.system(.title))
+                Spacer()
+                RoundedRectangle(cornerRadius: 10).fill(Color.gray)
+                    .offset(x: CGFloat(offSetX))
+                
+                Image(sneakers[segmentIndex])
+                    .resizable()
+                    .frame(width: 300,height: 300)
+                    .offset(x: CGFloat(offSetX))
+            }
+            Picker(selection: Binding(get: {
+                segmentIndex
+            }, set: { newValue in
+                print(newValue)
+            })) {
+                ForEach(0..<company.count) {
+                    Text(self.company[$0]).tag($0)
+                }
+            }
+        label: {
+            Text("")
+        }.pickerStyle(.segmented)
+                .padding()
+            Spacer().frame(height: 150)
         }
-        .padding()
     }
 }
 
